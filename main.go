0cb4c03e5a1ffff7a0dd601f260d761bf9cbbcf8
knanao <nao.7ken@gmail.com>
2019-05-01 15:01:02 +0900

    create form and query

--- main.go
+++ main.go
@@ -38,7 +38,12 @@ func main() {
 	e.Static("/public/img/", "./public/img")
 
 	e.GET("/", HandleIndexGet)
+	e.GET("/hello", HandleHelloGet)
+	e.POST("/hello", HandleHelloPost)
+	e.GET("/hello_form", HandleHelloFormGet)
+
 	e.GET("/api/hello", HandleAPIHelloGet)
+	e.POST("/api/hello", HandleAPIHelloPost)
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
@@ -51,14 +56,46 @@ func init() {
 func loadTemplates() {
 	var baseTemplate = "templates/layout.html"
 	templates = make(map[string]*template.Template)
-	templates["index"] = template.Must(
+	templates["hello"] = template.Must(
 		template.ParseFiles(baseTemplate, "templates/hello.html"))
+	templates["hello_form"] = template.Must(
+		template.ParseFiles(baseTemplate, "templates/hello_form.html"))
 }
 
 func HandleIndexGet(c echo.Context) error {
 	return c.Render(http.StatusOK, "index", "World")
 }
 
+func HandleHelloGet(c echo.Context) error {
+	greetingto := c.QueryParam("greetingto")
+	return c.Render(http.StatusOK, "hello", greetingto)
+}
+
+func HandleHelloPost(c echo.Context) error {
+	greetingto := c.FormValue("greetingto")
+	return c.Render(http.StatusOK, "hello", greetingto)
+}
+
+func HandleHelloFormGet(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello_form", nil)
+}
+
+// /api/hello のGet時のJSONデータ生成処理
 func HandleAPIHelloGet(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]interface{}{"hello": "world"})
+	greetingto := c.QueryParam("greetingto")
+	return c.JSON(http.StatusOK, map[string]interface{}{"hello": greetingto})
+}
+
+// /api/hello が受けとるJSONパラメータを定義
+type HelloParam struct {
+	GreetingTo string `json:"hello"`
+}
+
+// /api/hello のPost時のJSONデータ生成処理
+func HandleAPIHelloPost(c echo.Context) error {
+	param := new(HelloParam)
+	if err := c.Bind(param); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{"hello": param.GreetingTo})
 }
